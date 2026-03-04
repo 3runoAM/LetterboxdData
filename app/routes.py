389c69a3151e9a4133723d763bb458fcc618c2b0
Bruno Martins <jbrunno17@gmail.com>
2026-03-04 15:45:29 -0300

    Include data processing and graphics building capabilities

--- app/routes.py
+++ app/routes.py
@@ -1,5 +1,8 @@
 from flask import Blueprint, request, render_template, flash, redirect, url_for
+from .data_processing import load_processed_diary, load_processed_ratings
 from .file_processing import validate_files, save_files
+from .graph_builder import plot_volume_por_periodo, plot_dia_semana, plot_evolucao_gosto, plot_taxa_rewatch, \
+    plot_time_lag, get_max_streak, plot_distribuicao_notas, plot_decada_ouro
 
 main = Blueprint("main", __name__)
 
@@ -11,18 +14,13 @@ def upload_files_route():
 
 @main.route("/saveFiles", methods=["POST"])
 def save_files_route():
-    print("| Recebendo arquivos...")
     files = request.files.getlist("files")
-
-    print("| Validando arquivos...")
     valid, message = validate_files(files)
 
     if not valid:
-        print("| ERRO: " + message)
         flash(message)
         return redirect(url_for("main.upload_files_route"))
 
-    print("| Salvando arquivos...")
     save_files(files)
 
     flash("Arquivos salvos com sucesso")
@@ -32,4 +30,20 @@ def save_files_route():
 
 @main.route("/dashboard", methods=["GET"])
 def dashboard():
-    return render_template("dashboard.html")
+    df_diary, df_streaks = load_processed_diary()
+    df_rating = load_processed_ratings()
+
+    dia_semana = plot_dia_semana(df_diary)
+    evolucao_gosto = plot_evolucao_gosto(df_diary)
+    taxa_rewatch = plot_taxa_rewatch(df_diary)
+    time_lag = plot_time_lag(df_diary)
+    distribuicao_notas = plot_distribuicao_notas(df_rating)
+    decada_ouro = plot_decada_ouro(df_rating)
+
+    return render_template("dashboard.html",
+                           dia_semana=dia_semana,
+                           evolucao_gosto=evolucao_gosto,
+                           taxa_rewatch=taxa_rewatch,
+                           time_lag=time_lag,
+                           distribuicao_notas=distribuicao_notas,
+                           decada_ouro=decada_ouro)
