a773b8d5e3292557f25418923f9d433b71f3cd7f
Bruno Martins <jbrunno17@gmail.com>
2026-04-06 17:47:26 -0300

    Deleta colunas inuteis dos DFs e retorna lista de filmes junto do df_watched

--- app/data_processing.py
+++ app/data_processing.py
@@ -1,3 +1,4 @@
+import csv
 import os
 import pandas
 from .config import DATA_DIR, BASE_DIR
@@ -6,13 +7,15 @@ from .config import DATA_DIR, BASE_DIR
 def process_diary():
     df_diary = pandas.read_csv(os.path.join(DATA_DIR, "diary.csv"))
 
+    del df_diary['Letterboxd URI']
+    del df_diary['Tags']
+
     df_diary['Watched Date'] = pandas.to_datetime(df_diary['Watched Date'], errors='coerce')
     df_diary = df_diary.dropna(subset=['Watched Date']).copy()
 
     df_diary['Year'] = pandas.to_numeric(df_diary['Year'], errors='coerce')
     df_diary['Rewatch'] = df_diary['Rewatch'].fillna('No').map({'Yes': True, 'No': False})
 
-    # Todo: Criar script próprio para enriquecer os dados quando for se comunicar com a API ao invés de fazer isso na hora de carregar/tipar os dados
     df_diary['Ano_Mes'] = df_diary['Watched Date'].dt.to_period('M').astype(str)
     df_diary['Ano_Assistido'] = df_diary['Watched Date'].dt.year
     df_diary['Dia_da_Semana'] = df_diary['Watched Date'].dt.day_name()
@@ -20,38 +23,44 @@ def process_diary():
 
     return df_diary
 
-#-----------------------------------------------------------------------------------------------------------------------
+# -----------------------------------------------------------------------------------------------------------------------
 
 def process_ratings():
     df_ratings = pandas.read_csv(os.path.join(DATA_DIR, "ratings.csv"))
+
+    del df_ratings['Letterboxd URI']
 
     df_ratings['Rating'] = pandas.to_numeric(df_ratings['Rating'], errors='coerce')
     df_ratings['Year'] = pandas.to_numeric(df_ratings['Year'], errors='coerce')
 
     df_ratings = df_ratings.dropna(subset=['Rating', 'Year']).copy()
 
-    # Todo: Criar script próprio para enriquecer os dados quando for se comunicar com a API ao invés de fazer isso na hora de carregar/tipar os dados
     df_ratings['Decade'] = (df_ratings['Year'] // 10 * 10).astype(int).astype(str) + "s"
 
     return df_ratings
 
-#-----------------------------------------------------------------------------------------------------------------------
+# -----------------------------------------------------------------------------------------------------------------------
 
 def process_watched():
     df_watched = pandas.read_csv(os.path.join(DATA_DIR, "watched.csv"))
+
+    del df_watched['Letterboxd URI']
 
     df_watched['Date'] = pandas.to_datetime(df_watched['Date'], errors='coerce')
     df_watched = df_watched.dropna(subset=['Date']).copy()
 
     df_watched['Year'] = pandas.to_numeric(df_watched['Year'], errors='coerce')
 
-    return df_watched
+    movies = df_watched[['Name', 'Year']].drop_duplicates().dropna()
+    movies = movies.rename(columns={'Name': 'title', 'Year': 'release_year'}).to_dict(orient='records')
 
-#-----------------------------------------------------------------------------------------------------------------------
+    return df_watched, movies
+
+# -----------------------------------------------------------------------------------------------------------------------
 
 def get_processed_data():
     df_diary = process_diary()
     df_rating = process_ratings()
-    df_watched = process_watched()
+    df_watched, movies = process_watched()
 
-    return df_diary, df_rating, df_watched
+    return df_diary, df_rating, df_watched, movies
