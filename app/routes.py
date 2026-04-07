cb86111103b661277c42d0113651cf5c13674562
Bruno Martins <jbrunno17@gmail.com>
2026-04-07 19:04:19 -0300

    Implementa utilização dos data frames já enriquecidos na renderização

--- app/routes.py
+++ app/routes.py
@@ -6,7 +6,7 @@ from flask import Blueprint, request, render_template, flash, redirect, url_for
 from . import TMDB_Client
 from .feature_engine import get_context
 from .data_processing import process_diary, process_ratings, process_watched, get_processed_data
-from .file_handler import validate_files, save_files, is_data_available
+from .file_handler import validate_files, save_files, is_data_available, is_data_processed
 from .graph_builder import plot_favorite_day, plot_likeness_series, plot_rewatch_rate, \
     plot_time_lag, plot_rating_distribution, plot_favorite_decade
 from .TMDB_Client import TMDBClient
@@ -15,7 +15,7 @@ main = Blueprint("main", __name__)
 
 @main.route("/", methods=["GET"])
 def main_route():
-    if is_data_available():
+    if is_data_available() or is_data_processed():
         return redirect(url_for("main.perfil_route"))
     else:
         return render_template("upload.html")
@@ -41,32 +41,35 @@ def save_files_route():
 @main.route("/perfil", methods=["GET"])
 def perfil_route():
     try:
-        # PROCESSAMENTO E ENRIQUECIMENTO DOS DADOS
+        # PROCESSAMENTO
         df_diary, df_rating, df_watched, movies = get_processed_data()
 
-        client = TMDBClient(os.getenv("API_KEY"))
+        # ENRIQUECIMENTO
+        # client = TMDBClient(os.getenv("API_KEY"))
+        # enriched_data = client.fetch_movies_parallel(movies)
 
-        enriched_data = client.fetch_movies_parallel(movies)
-        df_enriched_data = pandas.DataFrame(enriched_data)
+        # df_enriched_data = pandas.DataFrame(enriched_data)
 
-        df_watched_enriched = df_watched.merge(df_enriched_data, how="left", on=["Name", "Year"]).dropna(subset="Genres")
+        df_watched_enriched = pandas.read_csv(os.path.join("data/dataFrames", "df_watched_enriched.csv"))
+
+        # df_watched_enriched = df_watched.merge(df_enriched_data, how="left", on=["Name", "Year"]).dropna(subset="Genres")
 
         # df_diary.to_csv("data/dataFrames/df_diary.csv", index=False)
         # df_rating.to_csv("data/dataFrames/df_rating.csv", index=False)
         # df_watched.to_csv("data/dataFrames/df_watched.csv", index=False)
         # df_watched_enriched.to_csv("data/dataFrames/df_watched_enriched.csv", index=False)
 
-        # GERANDO INSIGHTS
+        # GERANDO CONTEXTO
         context = get_context(df_watched, df_diary, df_rating, df_watched_enriched)
 
         # GERAÇÃO DOS GRÁFICOS COM PLOTLY
-        favorite_day = plot_favorite_day(df_diary)
-        likeness_series = plot_likeness_series(df_diary)
         rewatch_rate = plot_rewatch_rate(df_diary)
-        time_lag = plot_time_lag(df_diary)
-        rating_distribution = plot_rating_distribution(df_rating)
-        favorite_decade = plot_favorite_decade(df_rating)
 
+        # likeness_series = plot_likeness_series(df_diary)
+        # time_lag = plot_time_lag(df_diary)
+        # rating_distribution = plot_rating_distribution(df_rating)
+        # favorite_decade = plot_favorite_decade(df_rating)
+        # favorite_day = plot_favorite_day(df_diary)
     except FileNotFoundError:
         flash("Nenhum arquivo encontrado. Faça o upload dos arquivos necessários")
         return redirect(url_for("main.main_route"))
@@ -74,17 +77,12 @@ def perfil_route():
         print(f"Erro ao processar os dados: {e.with_traceback(e.__traceback__)}")
         return redirect(url_for("main.main_route"))
 
-    return render_template("perfil.html",
+    return render_template("profile.html",
                            context=context,
-                           favorite_day=favorite_day,
-                           likeness_series=likeness_series,
-                           rewatch_rate=rewatch_rate,
-                           time_lag=time_lag,
-                           rating_distribution=rating_distribution,
-                           favorite_decade=favorite_decade)
+                           rewatch_rate=rewatch_rate)
 
 #-----------------------------------------------------------------------------------------------------------------------
 
 @main.route("/perfilAtual", methods=["GET"])
 def perfil_atual():
-    return render_template("perfilAtual.html")
+    return render_template("currentProfile.html")
