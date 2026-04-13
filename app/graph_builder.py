9195235d80da38497a941c3ea16b88e28d58c3f0
Bruno Martins <111077073+3runoAM@users.noreply.github.com>
2026-04-13 17:47:46 -0300

    Criação e exibição de nuvem de palavras (#8)
    * Adiciona wordcloud lib e dependencias necessarias para criar uma word cloud
    
    * Adiciona novo método para criar imagem com wordcloud
    
    * Atualiza fluxo para gerar wordcloud caso não exista e atualiza métodos para seguir o padrão
    
    * Atualiza dashboard para exibir imagem com wordcloud e atualiza css e classes para melhor estilização

--- app/graph_builder.py
+++ app/graph_builder.py
@@ -1,39 +1,10 @@
+import os
+import numpy as np
 import plotly.express as px
 
-def plot_favorite_day(df_diary):
-    ordem_dias = ["Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday"]
-    nomes_br = ["Segunda", "Terça", "Quarta", "Quinta", "Sexta", "Sábado", "Domingo"]
-
-    volume = df_diary["Dia_da_Semana"].value_counts().reindex(ordem_dias).reset_index()
-    volume.columns = ["Dia", "Quantidade"]
-
-    volume["Dia"] = nomes_br
-
-    fig = px.bar(volume, x="Dia", y="Quantidade", title="Dia da Semana Favorito", color_discrete_sequence=["#00d94f"])
-    fig.update_layout(
-        xaxis_tickangle=-45,
-        paper_bgcolor="rgba(0,0,0,0)",
-        plot_bgcolor="rgba(0,0,0,0)",
-        font=dict(color="#fff")
-    )
-    return fig.to_html(full_html=False)
-
-
-def plot_likeness_series(df_diary):
-    evolucao = df_diary.groupby("Ano_Assistido")["Rating"].mean().reset_index()
-    fig = px.line(evolucao, x="Ano_Assistido", y="Rating", markers=True,
-                  title="Evolução do Gosto (Média de Notas por Ano)",
-                  color_discrete_sequence=["#f37b01"])
-
-    fig.update_layout(
-        xaxis_tickangle=-45,
-        paper_bgcolor="rgba(0,0,0,0)",
-        plot_bgcolor="rgba(0,0,0,0)",
-        font=dict(color="#fff"),
-        xaxis=dict(showgrid=False),
-        yaxis=dict(showgrid=True, gridcolor="rgba(255,255,255,0.1)"))
-
-    return fig.to_html(full_html=False)
+from app.config import STOPWORDS_OVERVIEW
+from wordcloud import WordCloud, STOPWORDS
+from PIL import Image, ImageOps
 
 
 def plot_rewatch_rate(df_diary):
@@ -58,61 +29,102 @@ def plot_rewatch_rate(df_diary):
     return fig.to_html(full_html=False)
 
 
-def plot_time_lag(df_diary):
-    lag_valido = df_diary[df_diary["Time_Lag"] >= 0]
+def plot_overview_wordcloud(df_watched_enriched):
+    try:
+        overviews_joined = " ".join(df_watched_enriched["Overview"].dropna())
 
-    fig = px.histogram(lag_valido, x="Time_Lag", nbins=50,
-                       title="Time Lag (Anos entre o Lançamento e a Visualização)",
-                       labels={"Time_Lag": "Anos de Atraso"},)
+        stopwords = set(STOPWORDS)
+        stopwords.update(STOPWORDS_OVERVIEW)
 
-    fig.update_layout(
-        xaxis_tickangle=-45,
-        paper_bgcolor="rgba(0,0,0,0)",
-        plot_bgcolor="rgba(0,0,0,0)",
-        font=dict(color="#fff"),
-        xaxis=dict(showgrid=False),
-        yaxis=dict(showgrid=True, gridcolor="rgba(255,255,255,0.1)")
-    )
+        image = Image.open("app/static/masks/letterboxd_logo_mask.png").convert("L")
 
-    fig.update_traces(marker_color="#00d650",
-                      marker_line_color="#fff",
-                      marker_line_width=1)
+        mask = np.array(image)
 
-    return fig.to_html(full_html=False)
+        wordcloud = WordCloud(
+            background_color=None,
+            mode="RGBA",
+            color_func=lambda *args, **kwargs: "#A9BBCC",
+            stopwords=stopwords,
+            max_font_size=100,
+            min_font_size=20,
+            mask=mask,
+            font_path="app/static/fonts/Yantramanav-Regular.ttf"
+        ).generate(overviews_joined.capitalize())
 
-def plot_rating_distribution(df_ratings):
-    dist = df_ratings["Rating"].value_counts().sort_index().reset_index()
-    dist.columns = ["Estrelas", "Quantidade"]
+        wordcloud.to_file("app/static/images/overview_cloud.png")
+    except Exception as e:
+        print(f"ERRO AO GERAR WORD CLOUD: {e.with_traceback(e.__traceback__)}")
 
-    fig = px.bar(dist, x="Estrelas", y="Quantidade",
-                 color_discrete_sequence=["#00B020"])
-
-    fig.update_xaxes(dtick=0.5)
-    fig.update_layout(
-        xaxis_tickangle=-45,
-        paper_bgcolor="rgba(0,0,0,0)",
-        plot_bgcolor="rgba(0,0,0,0)",
-        font=dict(color="#fff"),
-        xaxis=dict(showgrid=False),
-        yaxis=dict(showgrid=False, gridcolor="rgba(255,255,255,0.1)")
-    )
-
-    return fig.to_html(full_html=False)
-
-
-def plot_favorite_decade(df_ratings):
-    decadas = df_ratings.groupby("Decade")["Rating"].mean().reset_index()
-    fig = px.bar(decadas, x="Decade", y="Rating", title="Década de Ouro (Média de Nota por Década)",
-                 color="Rating", color_continuous_scale="Viridis")
-
-    fig.update_yaxes(range=[0, 5])
-    fig.update_layout(
-        xaxis_tickangle=-45,
-        paper_bgcolor="rgba(0,0,0,0)",
-        plot_bgcolor="rgba(0,0,0,0)",
-        font=dict(color="#fff"),
-
-        xaxis=dict(showgrid=False),
-        yaxis=dict(showgrid=True, gridcolor="rgba(255,255,255,0.1)")
-    )
-    return fig.to_html(full_html=False)
+# def plot_likeness_series(df_diary):
+#     evolucao = df_diary.groupby("Ano_Assistido")["Rating"].mean().reset_index()
+#     fig = px.line(evolucao, x="Ano_Assistido", y="Rating", markers=True,
+#                   title="Evolução do Gosto (Média de Notas por Ano)",
+#                   color_discrete_sequence=["#f37b01"])
+#
+#     fig.update_layout(
+#         xaxis_tickangle=-45,
+#         paper_bgcolor="rgba(0,0,0,0)",
+#         plot_bgcolor="rgba(0,0,0,0)",
+#         font=dict(color="#fff"),
+#         xaxis=dict(showgrid=False),
+#         yaxis=dict(showgrid=True, gridcolor="rgba(255,255,255,0.1)"))
+#
+#     return fig.to_html(full_html=False)
+#
+# def plot_time_lag(df_diary):
+#     lag_valido = df_diary[df_diary["Time_Lag"] >= 0]
+#
+#     fig = px.histogram(lag_valido, x="Time_Lag", nbins=50,
+#                        title="Time Lag (Anos entre o Lançamento e a Visualização)",
+#                        labels={"Time_Lag": "Anos de Atraso"},)
+#
+#     fig.update_layout(
+#         xaxis_tickangle=-45,
+#         paper_bgcolor="rgba(0,0,0,0)",
+#         plot_bgcolor="rgba(0,0,0,0)",
+#         font=dict(color="#fff"),
+#         xaxis=dict(showgrid=False),
+#         yaxis=dict(showgrid=True, gridcolor="rgba(255,255,255,0.1)")
+#     )
+#
+#     fig.update_traces(marker_color="#00d650",
+#                       marker_line_color="#fff",
+#                       marker_line_width=1)
+#
+#     return fig.to_html(full_html=False)
+#
+# def plot_rating_distribution(df_ratings):
+#     dist = df_ratings["Rating"].value_counts().sort_index().reset_index()
+#     dist.columns = ["Estrelas", "Quantidade"]
+#
+#     fig = px.bar(dist, x="Estrelas", y="Quantidade",
+#                  color_discrete_sequence=["#00B020"])
+#
+#     fig.update_xaxes(dtick=0.5)
+#     fig.update_layout(
+#         xaxis_tickangle=-45,
+#         paper_bgcolor="rgba(0,0,0,0)",
+#         plot_bgcolor="rgba(0,0,0,0)",
+#         font=dict(color="#fff"),
+#         xaxis=dict(showgrid=False),
+#         yaxis=dict(showgrid=False, gridcolor="rgba(255,255,255,0.1)")
+#     )
+#
+#     return fig.to_html(full_html=False)
+#
+# def plot_favorite_decade(df_ratings):
+#     decadas = df_ratings.groupby("Decade")["Rating"].mean().reset_index()
+#     fig = px.bar(decadas, x="Decade", y="Rating", title="Década de Ouro (Média de Nota por Década)",
+#                  color="Rating", color_continuous_scale="Viridis")
+#
+#     fig.update_yaxes(range=[0, 5])
+#     fig.update_layout(
+#         xaxis_tickangle=-45,
+#         paper_bgcolor="rgba(0,0,0,0)",
+#         plot_bgcolor="rgba(0,0,0,0)",
+#         font=dict(color="#fff"),
+#
+#         xaxis=dict(showgrid=False),
+#         yaxis=dict(showgrid=True, gridcolor="rgba(255,255,255,0.1)")
+#     )
+#     return fig.to_html(full_html=False)
