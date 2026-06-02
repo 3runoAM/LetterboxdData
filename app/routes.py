6474464d320f123d93e0070e996e1d25303584fd
Bruno Martins <jbrunno17@gmail.com>
2026-06-02 19:27:35 -0300

    Atualiza a rota para passar os graficos ao template

--- app/routes.py
+++ app/routes.py
@@ -2,10 +2,12 @@ from flask import Blueprint, request, render_template, flash, redirect, url_for
 
 from app.services.context_engine import get_profile_context, get_current_profile_context
 from app.services.etl_service import get_processed_data, transform_and_load
-from app.services.graph_builder import plot_overview_wordcloud, plot_rewatch_rate, plot_movie_map
+from app.services.graph_builder import plot_overview_wordcloud, plot_rewatch_rate, plot_movie_map, \
+    plot_time_lag_per_period
 from app.utils.file_handler import validate_files, save_files, is_data_available
 
 main = Blueprint("main", __name__)
+
 
 @main.route("/", methods=["GET"])
 def main_route():
@@ -13,6 +15,7 @@ def main_route():
         return render_template("upload.html")
     else:
         return redirect(url_for("main.profile_route"))
+
 
 # -----------------------------------------------------------------------------------------------------------------------
 
@@ -27,6 +30,7 @@ def save_files_route():
 
     save_files(files)
     return render_template("loading.html")
+
 
 # -----------------------------------------------------------------------------------------------------------------------
 
@@ -43,6 +47,7 @@ def process_data():
     except Exception as e:
         print(f"Data processing error: {e}")
         return {"status": "error", "message": str(e)}, 500
+
 
 # -----------------------------------------------------------------------------------------------------------------------
 
@@ -62,19 +67,25 @@ def profile_route():
         return redirect(url_for("main.main_route"))
 
 
-
 # -----------------------------------------------------------------------------------------------------------------------
 
 @main.route("/current-profile", methods=["GET"])
 def current_profile():
     try:
         context = get_current_profile_context()
+
+        time_lag_graphs = plot_time_lag_per_period(context.get("watched_movies"),
+                                                   [context.get("time_lag_week").get("time_lag_average"),
+                                                    context.get("time_lag_month").get("time_lag_average"),
+                                                    context.get("time_lag_year").get("time_lag_average")])
+
     except Exception as e:
         print(f"Data processing error: {e}")
         return redirect(url_for("main.main_route"))
 
     return render_template("currentProfile.html",
-                           context=context)
+                           context=context, time_lag_graphs=time_lag_graphs)
+
 
 # -----------------------------------------------------------------------------------------------------------------------
 
